73d6a4e83d7b383386f12844e2b4be2e39b48273
swati <swatinimalpudi@gmail.com>
2026-09-22 21:28:48 +0530

    updated froantend

---
+++ stp.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Hospital report generated successfully!");
+}
+
+function showInsight() {
+    alert(
+        "AI Insight: ICU energy consumption is 18% higher than the normal pattern. Consider checking HVAC equipment and temperature settings."
+    );
+}
